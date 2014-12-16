6aac734c62ec135a76f3ac0beaccff659090d33c
Alexander Oblovatniy <oblovatniy@gmail.com>
2014-12-16 20:19:47 +0200

    init autoreporter

--- __init__.py
+++ __init__.py
@@ -2,3 +2,36 @@
 """
 Create bug issues on GitHub automatically when an exception occurs.
 """
+
+import itertools
+
+from github import GitHub, ApiNotFoundError
+from Levenshtein.StringMatcher import StringMatcher
+
+from .constants import (
+    DuplicateIssueLabels, InvalidIssueLabels, NewIssueLabels,
+)
+
+
+class BugAutoreporter(object):
+
+    def __init__(self, access_token, repo_owner, repo_name):
+        gh = GitHub(access_token=access_token)
+        self.api = gh.repos(repo_owner)(repo_name)
+        self._ensure_labels_exist()
+
+        self.matcher = StringMatcher()
+
+    def _ensure_labels_exist(self):
+        labels = itertools.chain(*[
+            container.constants()
+            for container in (
+                DuplicateIssueLabels, InvalidIssueLabels, NewIssueLabels,
+            )
+        ])
+        labels_api = self.api('labels')
+        for label in labels:
+            try:
+                labels_api(label.title).get()
+            except ApiNotFoundError:
+                labels_api.post(name=label.title, color=label.color)
---
+++ constants.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from candv import SimpleConstant, Constants, with_constant_class
+
+
+class Label(SimpleConstant):
+
+    def __init__(self, title, hex_color):
+        super(Label, self).__init__()
+        self.title = title
+        self.color = hex_color
+
+
+class Labels(with_constant_class(Label), Constants):
+
+    @classmethod
+    def titles(cls):
+        return [c.title for c in cls.constants()]
+
+
+class DuplicateIssueLabels(Labels):
+    DUPLICATE = Label('duplicate', 'cccccc')
+
+
+class InvalidIssueLabels(Labels):
+    INVALID = Label('invalid', 'e6e6e6')
+    WONTFIX = Label('wontfix', 'ffffff')
+
+
+class NewIssueLabels(Labels):
+    BUG = Label('bug', 'e11d21')
+    AUTO_REPORT = Label('auto-report', 'fbca04')
